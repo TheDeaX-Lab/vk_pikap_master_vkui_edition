2df50e8f3f65bfdb84208b804707c3fcaec75513
TerryChan2003 <aleksandr_h2001@mail.ru>
2019-11-03 12:48:56 +0300

    deploy

---
+++ precache-manifest.8b14835e41799bfabe0e453e49dbc2cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5163f583393d2c1a43059ffdc7d0fb8",
+    "url": "/vk_pikap_master_vkui_edition/index.html"
+  },
+  {
+    "revision": "7834c6e2e7fd1ea0cb90",
+    "url": "/vk_pikap_master_vkui_edition/static/css/2.27cde75d.chunk.css"
+  },
+  {
+    "revision": "49b0b813b9b99ee625e5",
+    "url": "/vk_pikap_master_vkui_edition/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7834c6e2e7fd1ea0cb90",
+    "url": "/vk_pikap_master_vkui_edition/static/js/2.371c091f.chunk.js"
+  },
+  {
+    "revision": "49b0b813b9b99ee625e5",
+    "url": "/vk_pikap_master_vkui_edition/static/js/main.ee437b36.chunk.js"
+  },
+  {
+    "revision": "6aeca3599148bff95468",
+    "url": "/vk_pikap_master_vkui_edition/static/js/runtime-main.5e77a663.js"
+  },
+  {
+    "revision": "f9053c3d5e260b1103fd87538dfb378a",
+    "url": "/vk_pikap_master_vkui_edition/static/media/VcH4TT4bFkw.f9053c3d.png"
+  },
+  {
+    "revision": "d67d8ab4f4c10bf22aa353e27879133c",
+    "url": "/vk_pikap_master_vkui_edition/static/media/version.d67d8ab4.txt"
+  }
+]);
